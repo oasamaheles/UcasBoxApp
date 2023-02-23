f424baabc61f4361a69c9991bdc4b233d60d330b
ohill <osamaheles12345@gmail.com>
2023-02-23 12:17:54 +0200

    Final App Box Color

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -10,16 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -154,11 +151,12 @@ fun SimpleColumn() {
 fun DefaultPreview() {
     SimpleColumn()
 }
+
 @Composable
-fun TextBox(text: String){
+fun TextBox(text: String) {
     Text(
         text = text,
-        modifier=Modifier.padding(Dp(10f)),
+        modifier = Modifier.padding(Dp(10f)),
 //        modifier=Modifier.padding(start = 10.dp, top = 10.dp),
         style = TextStyle(
             color = Color.Gray,
